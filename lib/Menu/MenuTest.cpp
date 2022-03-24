7db4d73fa93b93bbc7ff899054169c028d07a4ab
acomarcho <acoxstpd@gmail.com>
2022-03-24 18:02:15 +0700

    added unit test for Menu

---
+++ lib/Menu/MenuTest.cpp
@@ -0,0 +1,20 @@
+#include "Menu.hpp"
+
+int main() {
+  Menu menu;
+  ItemsReader items = ItemsReader("config/item.txt");
+  RecipesReader recipes = RecipesReader("config/recipe");
+  menu.Show();
+  menu.give(items, "OAK_PLANK", 1);
+  menu.give(items, "BIRCH_PLANK", 2);
+  menu.give(items, "STICK", 1);
+  menu.MoveToCraft("I0", 1, {"C0"});
+  menu.MoveToCraft("I1", 1, {"C3"});
+  menu.Craft(items, recipes);
+  menu.Discard("I2", 2);
+  menu.Show();
+  menu.give(items, "WOODEN_SWORD", 1);
+  menu.Use("I0");
+  menu.Show();
+  menu.exportInventory(items, "./menuTest.out");
+}
