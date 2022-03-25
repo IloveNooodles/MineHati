7fde51b65e55810d69bd9b4901a8992674d812ea
Rifqi Naufal Abdjul <rifqi.abdjul23@gmail.com>
2022-03-25 21:22:17 +0700

    remove unnecessary method to increase coverage

--- lib/Items/item.cpp
+++ lib/Items/item.cpp
@@ -1,44 +1,20 @@
 #include "item.hpp"
 
-Item::Item()
-{
+Item::Item() {
   this->id = -999;
   this->name = "-";
   this->category = "-";
 }
-Item::Item(int id, string nama, string jenis)
-{
+Item::Item(int id, string nama, string jenis) {
   this->id = id;
   this->name = nama;
   this->category = jenis;
 }
-Item::Item(int id, string nama)
-{
+Item::Item(int id, string nama) {
   this->id = id;
   this->name = nama;
   this->category = "-";
 }
-int Item::getId() const
-{
-  return this->id;
-}
-void Item::setId(int id)
-{
-  this->id = id;
-}
-string Item::getName() const
-{
-  return this->name;
-}
-void Item::setName(string name)
-{
-  this->name = name;
-}
-string Item::getCategory() const
-{
-  return this->category;
-}
-void Item::setCategory(string category)
-{
-  this->category = category;
-}
+int Item::getId() const { return this->id; }
+string Item::getName() const { return this->name; }
+string Item::getCategory() const { return this->category; }
